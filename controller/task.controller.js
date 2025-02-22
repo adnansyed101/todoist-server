fc13ce93aae895ca475b48db4512a0487230a1ff
Adnan <sirgeant10@gmail.com>
2025-02-22 13:43:29 +0600

    Add controller to update task status and position.

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Task from "../models/task.model.js";
 
 export const createTask = async (req, res) => {
@@ -34,6 +35,29 @@ export const getAllUserTask = async (req, res) => {
     res.status(201).json(tasks);
   } catch (err) {
     console.log("Error in fetching task: " + err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const updateTaskPosition = async (req, res) => {
+  const { taskId } = req.params;
+  const { status, position } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(taskId)) {
+    return res.status(500).json({ success: false, message: "Invalid Task Id" });
+  }
+
+  const updated = {
+    $set: { status, position },
+  };
+
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(taskId, updated, {
+      new: true,
+    });
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.log("Error in updating task: " + err.message);
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
